4bb165b32dc8fa70b9a2182ab651d6898c08a620
Iconic <muhammadtaimoor2006@gmail.com>
2023-08-13 14:24:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemeu.style.right = "0px";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
